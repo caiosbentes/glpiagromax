169d39d85ecf973da868064bd3203c421c658ecf
Caio Sanches Bentes <caiosbentes@users.noreply.github.com>
2018-08-02 09:23:58 -0300

    final
    final

--- glpi.sh
+++ glpi.sh
@@ -41,12 +41,57 @@ apt-get install -y 	ca-certificates \
 				php-cas \
 				mariadb-server
 
-chown -R www-data:www-data /var/www/html/
-chmod -Rf 775 /var/www/html
 read -sp "Qual a senha do root do banco de dados? " ROOTDBPWD
 echo
 debconf-set-selections <<< "mariadb-server-10.0 mariadb-server/root_password password $ROOTDBPWD"
 debconf-set-selections <<< "mariadb-server-10.0 mariadb-server/root_password_again password $ROOTDBPWD"
 debconf-set-selections <<< "mariadb-server-10.0 mariadb-server/oneway_migration boolean true"
+echo "create database glpi;" | mysql -uroot
+echo "create user glpi@localhost identified by '123456';" | mysql -uroot
+echo "grant all on glpi.* to glpi identified by '123456';" | mysql -uroot
+mysql -h localhost -u glpi -p glpi < glpi.sql
 
+cp -r glpi /var/www/html/
+chown -R www-data:www-data /var/www/html/
+chmod -Rf 775 /var/www/html
 
+cat <<EOF > /etc/apache2/sites-enabled/000-default.conf
+<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+        Alias /log/ "/var/log/"
+   	<Directory "/var/log/">
+	       Options Indexes MultiViews FollowSymLinks
+	       AllowOverride None
+	       Order deny,allow
+	       Deny from all
+	       Allow from all
+	        Require all granted
+   	</Directory>
+</VirtualHost>
+EOF
